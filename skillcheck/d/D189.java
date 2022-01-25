fbec177f8340c6dd6aba4bffa12c8d297df4961a
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-01-25 21:05:01 +0900

    Create java scripts

---
+++ skillcheck/d/D189.java
@@ -0,0 +1,15 @@
+package skillcheck.d;
+
+import java.util.*;
+
+
+public class D189 {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(y - x);
+    }
+}
